6e2fd1c43e7f6629835285e311f92b5674ec0198
Princy <princy@intertoons.com>
2021-07-28 18:27:11 +0530

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="text-center ">
+  <nuxt-link to="/">home</nuxt-link>
+  <nuxt-link to="about">about</nuxt-link>
+</div>
+  
+</template>
+
+<script>
+export default {
+    async asyncData ({ query, store }) {
+       let pageinfo ={
+           title:'about page dynamic',
+           discription:'about page discription',
+           ogImage:'http://wpr.intertoons.net/centrealadmin/assets/images/uploads/productimages/Dabur-Chyawanprash-2Ximmunity-500G-Centreal-Bazaar-Supermarket-in-Kerala.jpg'
+       }
+
+         return {
+         pageinfo
+      }
+    },
+    head() {
+      return {
+        title: this.pageinfo.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: this.pageinfo.discription
+          },
+          {
+                hid: 'og:title',
+                name: 'og:title',
+                content: this.pageinfo.title,
+            },
+            {
+                hid: 'og:image',
+                property: 'og:image',
+                content: this.pageinfo.ogImage,
+            },
+            {
+                hid: 'og:description',
+                property: 'og:description',
+                content: this.pageinfo.description,
+            },
+            
+        ]
+      }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="text-center ">
+  <nuxt-link to="/">home</nuxt-link>
+  <nuxt-link to="about">about</nuxt-link>
+</div>
+  
+</template>
+
+<script>
+export default {
+   layout: 'default',
+   head() {
+      return {
+        title: 'home page',
+        meta: [
+          {
+            hid: 'description',
+            name: 'home page discription',
+            content: 'Home page description'
+          }
+        ]
+      }
+    }
+}
+</script>
